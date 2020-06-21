e933f60915618187f980dc754ccf686b1a00ee2e
Dino Wernli <dinowernli@gmail.com>
2020-06-21 13:31:38 +0100

    Add a basic proto file for the raft protocol.

---
+++ build.rs
@@ -0,0 +1,8 @@
+fn main() {
+  protoc_rust_grpc::Codegen::new()
+    .out_dir("src")
+    .input("src/raft.proto")
+    .rust_protobuf(true)
+    .run()
+    .expect("protoc-rust-grpc");
+}
---
+++ src/raft_grpc.rs
@@ -0,0 +1,103 @@
+// This file is generated. Do not edit
+// @generated
+
+// https://github.com/Manishearth/rust-clippy/issues/702
+#![allow(unknown_lints)]
+#![allow(clippy::all)]
+
+#![cfg_attr(rustfmt, rustfmt_skip)]
+
+#![allow(box_pointers)]
+#![allow(dead_code)]
+#![allow(missing_docs)]
+#![allow(non_camel_case_types)]
+#![allow(non_snake_case)]
+#![allow(non_upper_case_globals)]
+#![allow(trivial_casts)]
+#![allow(unsafe_code)]
+#![allow(unused_imports)]
+#![allow(unused_results)]
+
+
+// server interface
+
+pub trait RaftServer {
+    fn vote(&self, o: ::grpc::ServerHandlerContext, req: ::grpc::ServerRequestSingle<super::raft::VoteRequest>, resp: ::grpc::ServerResponseUnarySink<super::raft::VoteResponse>) -> ::grpc::Result<()>;
+
+    fn append(&self, o: ::grpc::ServerHandlerContext, req: ::grpc::ServerRequestSingle<super::raft::AppendRequest>, resp: ::grpc::ServerResponseUnarySink<super::raft::AppendResponse>) -> ::grpc::Result<()>;
+}
+
+// client
+
+pub struct RaftServerClient {
+    grpc_client: ::std::sync::Arc<::grpc::Client>,
+}
+
+impl ::grpc::ClientStub for RaftServerClient {
+    fn with_client(grpc_client: ::std::sync::Arc<::grpc::Client>) -> Self {
+        RaftServerClient {
+            grpc_client: grpc_client,
+        }
+    }
+}
+
+impl RaftServerClient {
+    pub fn vote(&self, o: ::grpc::RequestOptions, req: super::raft::VoteRequest) -> ::grpc::SingleResponse<super::raft::VoteResponse> {
+        let descriptor = ::grpc::rt::ArcOrStatic::Static(&::grpc::rt::MethodDescriptor {
+            name: ::grpc::rt::StringOrStatic::Static("/raft.RaftServer/Vote"),
+            streaming: ::grpc::rt::GrpcStreaming::Unary,
+            req_marshaller: ::grpc::rt::ArcOrStatic::Static(&::grpc_protobuf::MarshallerProtobuf),
+            resp_marshaller: ::grpc::rt::ArcOrStatic::Static(&::grpc_protobuf::MarshallerProtobuf),
+        });
+        self.grpc_client.call_unary(o, req, descriptor)
+    }
+
+    pub fn append(&self, o: ::grpc::RequestOptions, req: super::raft::AppendRequest) -> ::grpc::SingleResponse<super::raft::AppendResponse> {
+        let descriptor = ::grpc::rt::ArcOrStatic::Static(&::grpc::rt::MethodDescriptor {
+            name: ::grpc::rt::StringOrStatic::Static("/raft.RaftServer/Append"),
+            streaming: ::grpc::rt::GrpcStreaming::Unary,
+            req_marshaller: ::grpc::rt::ArcOrStatic::Static(&::grpc_protobuf::MarshallerProtobuf),
+            resp_marshaller: ::grpc::rt::ArcOrStatic::Static(&::grpc_protobuf::MarshallerProtobuf),
+        });
+        self.grpc_client.call_unary(o, req, descriptor)
+    }
+}
+
+// server
+
+pub struct RaftServerServer;
+
+
+impl RaftServerServer {
+    pub fn new_service_def<H : RaftServer + 'static + Sync + Send + 'static>(handler: H) -> ::grpc::rt::ServerServiceDefinition {
+        let handler_arc = ::std::sync::Arc::new(handler);
+        ::grpc::rt::ServerServiceDefinition::new("/raft.RaftServer",
+            vec![
+                ::grpc::rt::ServerMethod::new(
+                    ::grpc::rt::ArcOrStatic::Static(&::grpc::rt::MethodDescriptor {
+                        name: ::grpc::rt::StringOrStatic::Static("/raft.RaftServer/Vote"),
+                        streaming: ::grpc::rt::GrpcStreaming::Unary,
+                        req_marshaller: ::grpc::rt::ArcOrStatic::Static(&::grpc_protobuf::MarshallerProtobuf),
+                        resp_marshaller: ::grpc::rt::ArcOrStatic::Static(&::grpc_protobuf::MarshallerProtobuf),
+                    }),
+                    {
+                        let handler_copy = handler_arc.clone();
+                        ::grpc::rt::MethodHandlerUnary::new(move |ctx, req, resp| (*handler_copy).vote(ctx, req, resp))
+                    },
+                ),
+                ::grpc::rt::ServerMethod::new(
+                    ::grpc::rt::ArcOrStatic::Static(&::grpc::rt::MethodDescriptor {
+                        name: ::grpc::rt::StringOrStatic::Static("/raft.RaftServer/Append"),
+                        streaming: ::grpc::rt::GrpcStreaming::Unary,
+                        req_marshaller: ::grpc::rt::ArcOrStatic::Static(&::grpc_protobuf::MarshallerProtobuf),
+                        resp_marshaller: ::grpc::rt::ArcOrStatic::Static(&::grpc_protobuf::MarshallerProtobuf),
+                    }),
+                    {
+                        let handler_copy = handler_arc.clone();
+                        ::grpc::rt::MethodHandlerUnary::new(move |ctx, req, resp| (*handler_copy).append(ctx, req, resp))
+                    },
+                ),
+            ],
+        )
+    }
+}
