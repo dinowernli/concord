3e9895929b95feb5fee77573d018c60af554c5b1
Dino Wernli <dinowernli@gmail.com>
2020-07-25 22:47:03 +0100

    Add tests for diagnostics and correctly handle skipping terms.

--- src/diagnostics.rs
+++ src/diagnostics.rs
@@ -50,49 +50,47 @@ impl Diagnostics {
     // there should be no term for which two servers recognize different peers
     // as the leader of the cluster.
     fn validate_leaders(&mut self) -> Result<(), String> {
-        // TODO(dino): Start at -1 instead of 0 and add proper support for the case
-        // where terms may have no leader at all (like term 0).
         let latest = match self.leaders.last_entry() {
             Some(e) => *e.key(),
-            None => 0,
+            None => -1,
         };
 
         let mut term = latest;
         loop {
             term = term + 1;
 
-            let mut candidate_opt: Option<Server> = None;
+            let mut candidate: Option<Server> = None;
             for (_, server) in &self.servers {
                 let s = server.lock().unwrap();
-
-                let leader_opt = s.leaders.get(&term).clone();
-                if leader_opt.is_none() {
-                    // This server hasn't found a leader for this term yet. Stop.
+                if s.latest_term().unwrap_or(-1) < term {
+                    // This server hasn't seen a leader for this term yet. Stop.
                     return Ok(());
                 }
 
+                let leader_opt = s.leaders.get(&term).clone();
+                if leader_opt.is_none() {
+                    // This server has seen a leader for a later term, but not this one.
+                    continue;
+                }
+
                 let leader = leader_opt.unwrap();
-                match candidate_opt.clone() {
-                    None => candidate_opt = Some(leader.clone()),
-                    Some(candidate) => {
-                        if &candidate != leader {
+                match candidate.clone() {
+                    None => candidate = Some(leader.clone()),
+                    Some(c) => {
+                        if &c != leader {
                             return Err(format!("Incompatible leader for term: {}", term));
                         }
                     }
                 }
             }
 
-            if candidate_opt.is_none() {
-                return Ok(());
-            }
-
-            // All servers agree on the leader for this term, store it.
-            let candidate = candidate_opt.unwrap().clone();
-            self.leaders.insert(term, candidate.clone());
+            // At this point, all servers have moved on to seeing leaders for a future
+            // term. Note that we may or may not have an actual leader.
             info!(
                 "Validated agreed leader for term {} to be {:?}",
                 term, &candidate
             );
+            candidate.map(|c| self.leaders.insert(term, c.clone()));
         }
     }
 }
@@ -101,13 +99,13 @@ impl Diagnostics {
 // cluster over time.
 pub struct ServerDiagnostics {
     // Keeps track of the leader for each term.
-    leaders: HashMap<i64, Server>,
+    leaders: BTreeMap<i64, Server>,
 }
 
 impl ServerDiagnostics {
     fn new() -> Self {
         ServerDiagnostics {
-            leaders: HashMap::new(),
+            leaders: BTreeMap::new(),
         }
     }
 
@@ -117,8 +115,105 @@ impl ServerDiagnostics {
         assert!(existing.is_none() || existing.unwrap() == leader);
         self.leaders.insert(term, leader.clone());
     }
+
+    fn latest_term(&self) -> Option<i64> {
+        self.leaders.last_key_value().map(|(k, _)| k.clone())
+    }
 }
 
 fn address_key(address: &Server) -> String {
     format!("{}:{}", address.get_host(), address.get_port())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    struct Fixture {
+        s1: Server,
+        s2: Server,
+        s3: Server,
+        diag: Diagnostics,
+    }
+
+    impl Fixture {
+        fn new() -> Self {
+            Fixture {
+                s1: make_server("foo", 123),
+                s2: make_server("bar", 123),
+                s3: make_server("baz", 123),
+                diag: Diagnostics::new(),
+            }
+        }
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_catch_inconsistent_single_server() {
+        let mut f = Fixture::new();
+
+        let d1 = f.diag.get_server(&f.s1);
+        d1.lock().unwrap().report_leader(1, &f.s1);
+        d1.lock().unwrap().report_leader(1, &f.s2);
+    }
+
+    #[test]
+    fn test_validate_happy() {
+        let mut f = Fixture::new();
+
+        let d1 = f.diag.get_server(&f.s1);
+        let d2 = f.diag.get_server(&f.s2);
+
+        d1.lock().unwrap().report_leader(2, &f.s1);
+        d2.lock().unwrap().report_leader(2, &f.s1);
+
+        f.diag.validate().expect("validation should succeed");
+    }
+
+    #[test]
+    fn test_validate_failure() {
+        let mut f = Fixture::new();
+
+        let d1 = f.diag.get_server(&f.s1);
+        let d2 = f.diag.get_server(&f.s2);
+        let d3 = f.diag.get_server(&f.s3);
+
+        d1.lock().unwrap().report_leader(1, &f.s1);
+        d2.lock().unwrap().report_leader(1, &f.s1);
+        d3.lock().unwrap().report_leader(1, &f.s1);
+
+        d1.lock().unwrap().report_leader(2, &f.s2);
+        d2.lock().unwrap().report_leader(2, &f.s2);
+        d3.lock().unwrap().report_leader(2, &f.s3);
+
+        assert!(f.diag.validate().is_err());
+    }
+
+    #[test]
+    fn test_validate_skips_gaps() {
+        let mut f = Fixture::new();
+
+        let d1 = f.diag.get_server(&f.s1);
+        let d2 = f.diag.get_server(&f.s2);
+        let d3 = f.diag.get_server(&f.s3);
+
+        d1.lock().unwrap().report_leader(1, &f.s1);
+        d2.lock().unwrap().report_leader(1, &f.s1);
+        d3.lock().unwrap().report_leader(1, &f.s1);
+
+        // Whole bunch of missing terms, then a conflict.
+
+        d1.lock().unwrap().report_leader(6, &f.s2);
+        d2.lock().unwrap().report_leader(6, &f.s2);
+        d3.lock().unwrap().report_leader(6, &f.s3);
+
+        assert!(f.diag.validate().is_err());
+    }
+
+    fn make_server(host: &str, port: i16) -> Server {
+        let mut result = Server::new();
+        result.set_host(host.to_string());
+        result.set_port(port as i32);
+        return result;
+    }
 }
